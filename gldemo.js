97aebaf48b905765915a8fb7abeed8877a1e0f43
Daniel Ching <daniel.j.ching@gmail.com>
2015-07-29 19:41:20 -0700

    Restructured and added framework to process point clouds.

--- gldemo.js
+++ gldemo.js
@@ -1,42 +1,59 @@
 var scene, camera, renderer;
-var geometry, material, mesh;
+var geometry, material, cloud;
+
+function loadCloudGeometry(objname) {
+	var ageometry = new THREE.BoxGeometry( 1, 1, 1 );
+	//ageometry.vertices.push(new THREE.Vector3(0, 0, 0),
+		//											new THREE.Vector3(0, 1, 0),
+			//										new THREE.Vector3(0, 0, 1));
+	//var colors = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0];
+
+	//ageometry.colors = colors;
+	return ageometry;
+}
 
 function init() {
+	HEIGHT = window.innerWidth;
+	WIDTH = window.innerWidth;
+
+	// Set up Light, Camera, and Scene
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = 1000;
-
-	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	var light = new THREE.AmbientLight( 0xffffff ); // soft white light
 	scene.add( light );
 
-	geometry = new THREE.BoxGeometry( 200, 200, 200 );
-	material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+	camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 1, 10000 );
+	camera.position.z = 4;
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+	// Load objects
 
+	geometry = loadCloudGeometry('/obj/bunny.obj');
+	material = new THREE.PointCloudMaterial();
+	cloud = new THREE.PointCloud(geometry, material);
+	scene.add(cloud);
+
+
+	// Render
 	renderer = new THREE.WebGLRenderer();
-	//renderer.setSize( canvas.width, canvas.height );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setClearColor( 0xb4b4b4, 1);
+	renderer.setSize( WIDTH, HEIGHT );
+	renderer.setClearColor( 0x000000, 1);
 
 	var canvas = document.getElementById("glcanvas");
 	canvas.appendChild(renderer.domElement);
-}
+};
 
 function animate() {
 
 	requestAnimationFrame( animate );
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+	cloud.rotation.x += 0.01;
+	cloud.rotation.y += 0.02;
 
 	renderer.render( scene, camera );
 
-}
+};
 
 function startgl() {
 	init();
 	animate();
-}
+};
