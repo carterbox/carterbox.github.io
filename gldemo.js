b5ab6280fe85b08451b52ff782a58be3b31ec357
Daniel Ching <daniel.j.ching@gmail.com>
2015-07-30 13:02:35 -0700

    Added mouse interation via THREE.OrbitControls.

--- gldemo.js
+++ gldemo.js
@@ -1,6 +1,6 @@
 var scene, camera, renderer;
-var geometry, material, cloud;
-var rotateflag = false;
+var clouds = [];
+var container;
 
 function init() {
 	HEIGHT = 480;//window.innerWidth;
@@ -15,47 +15,62 @@ function init() {
 	camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 1, 10000 );
 	camera.position.z = 4;
 
-	// Load objects
+	// Controls view interaction.
+	controls = new THREE.OrbitControls( camera );
+	controls.damping = 0.2;
+	controls.addEventListener( 'change', render );
+
+	// Load object(s)
 	var loader = new THREE.cOBJLoader();
-	geometry = loader.parse('http://people.oregonstate.edu/~chingd/obj/bunny.obj');
-	material = new THREE.PointCloudMaterial( {color: 0x9933ff, size:0.001});
-	cloud = new THREE.PointCloud(geometry, material);
-	scene.add(cloud);
+	var geometry = loader.parse('http://people.oregonstate.edu/~chingd/obj/bunny.obj');
+	var material = new THREE.PointCloudMaterial( {color: 0x9933ff, size:0.001});
+	clouds[0] = new THREE.PointCloud(geometry, material);
+	scene.add(clouds[0]);
 
 	// Render
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( WIDTH, HEIGHT );
 	renderer.setClearColor( 0x000000, 1);
 
-	var canvas = document.getElementById("glcanvas");
-	canvas.appendChild(renderer.domElement);
+	container = document.getElementById("glcanvas");
+	container.appendChild(renderer.domElement);
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	animate();
 };
+
+function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		render();
+}
 
 function animate() {
 	requestAnimationFrame( animate );
+	controls.update();
+	render();
+}
+
+function render() {
 	renderer.render( scene, camera );
-	if (rotateflag) {
-		cloud.rotation.x += 0.01;
-	}
-
-
-};
+}
 
 function startgl() {
 	init();
 	animate();
-};
+}
 
 var main = function() {
 	$(document).keypress(function(event)	{
 		if(event.which === 114) {
-			// r toggle cloud rotation.
-			rotateflag = rotateflag === false;
+			// r toggle camera rotation.
+			controls.autoRotate = controls.autoRotate === false;
 		}
 		else if(event.which === 104) {
 			// h hides the cloud.
-			cloud.visible = cloud.visible === false;
+			clouds[0].visible = clouds[0].visible === false;
 		}
 	});
-
 };
