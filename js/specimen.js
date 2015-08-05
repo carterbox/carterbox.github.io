e5f6b7587eb8ac5a96836a2664aaf3c084d72e31
Daniel Ching <daniel.j.ching@gmail.com>
2015-08-05 00:35:49 -0700

    Set up framework for loading specimen data from JSON file.

--- js/specimen.js
+++ js/specimen.js
@@ -1,10 +1,5 @@
-var scene, camera, renderer;
-var clouds = [];
-var container;
-var HEIGHT = 500;
-
 function changestep(step) {
-	// Loads the data for the clicked preview.
+	// Loads the data for the clicked step preview.
 	$('.previews img').toggleClass('active');
 	var newdir = step.attr("id");
 
@@ -15,13 +10,14 @@ function changestep(step) {
 }
 
 function pad(num, size) {
+	// Padds a NUMber to SIZE places with leading zeros.
     var s = num + "";
     while (s.length < size) s = "0" + s;
     return s;
 }
 
 function changeview(angle) {
-	// Loads the viewing angle for the slider.
+	// Loads the viewing angle from the slider input.
 	var index = pad(angle/10,2);
 
 	$('.big-image img').each( function() {
@@ -30,7 +26,30 @@ function changeview(angle) {
 	});
 }
 
+function changedata(step) {
+	// Loads speciment data into the table
+
+}
+
+function init() {
+
+	// Replace the specimen name
+
+	// Load the json file from the directory
+	sampleData = {};
+
+	// Replace all the constant text
+
+	// Remove extra step thumbs
+
+	// Replace step variable data
+	changedata('step01');
+
+}
+
 var main = function() {
+
+	// Big Image control buttons
 	$('.auto-rotate-button').click(function() {
 		// Toggle auto rotation
 	});
@@ -52,9 +71,8 @@ var main = function() {
 	$('.adhesive-button').click(function() {
 			$('.adhesive-button').toggleClass('active');
 	});
-	$('.previews img').click(function() {
-		changestep($(this));
-	});
+
+	// Slider Functions
 	$( "#slider" ).slider({
 		value: 0,
 		min: 0,
@@ -66,8 +84,18 @@ var main = function() {
 		}
 	});
 	$( "#view-angle" ).val($( "#slider" ).slider( "value" ) );
+
+	// Step previews
+	$('.previews img').click(function() {
+		changestep($(this));
+		//changedata($(this));
+	});
+
+	// Initialize image links and load specimen information.
 	$('img').each( function() {
 		$(this).attr('src', $(this).attr("src").replace(/SPECIMEN/g, urlParams["sample"]));
 		console.log($(this).attr("src"));
 	});
+
+	//init();
 };
